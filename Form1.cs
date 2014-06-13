50a340739142cf02c6049e181643dd7c704543ec
unknown <erin314@naver.com>
2014-06-13 13:09:26 +0900

    fourth

---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        private Mainfunction block = new Mainfunction();
+        int time = 0;
+        int move = 0;
+        int bottom = 0;
+        private int[,] map = new int[17,10]
+        {
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {7,7,7,7,7,7,7,7,7,7}
+        };
+
+        private Bitmap[] tiles = new Bitmap[]
+        {
+            Properties.Resources.그림1,
+            Properties.Resources.그림2,
+            Properties.Resources.그림3,
+            Properties.Resources.그림4,
+            Properties.Resources.그림5,
+            Properties.Resources.그림6,
+            Properties.Resources.그림7,
+            Properties.Resources.그림8
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            //배경
+            for (int row = 0; row < map.GetLength(0); row++)
+            {
+                for (int col = 0; col < map.GetLength(1); col++)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Brushes.DeepSkyBlue), 20, 20, 120, 190);
+
+                    e.Graphics.DrawImage(tiles[map[row, col]],
+                        new Rectangle(col * 12 + 20, row * 12 + 20, 12, 12));
+                }
+            }
+            //타일
+                for (int a = 0; a < 4; a++)
+                {
+                    for (int b = 0; b < 4; b++)
+                    {
+                        if (block.mainB[a, b] == 0) { continue; }
+                        e.Graphics.DrawImage(tiles[block.mainB[a, b]],
+                            new Rectangle((block.i + b) * 12 + 20,(block.j+time-4 + a) * 12 + 20, 12, 12));
+                    }
+                }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (block.mainB[0, 0] == -1)
+            { block = new Mainfunction(); }
+            int check = block.check(map, time, move, bottom);
+
+            if (check == 2)
+            {
+                
+                block.put(ref map, time, move);
+                block = new Mainfunction();
+                block.mainB[0, 0] = -1;
+                time = 0;
+                bottom = 0;
+
+            }
+            else if (check == 1)
+            {
+                bottom++;
+                time++;
+            }
+            else
+            {
+                bottom = 0;
+                time++;
+            }
+
+            Invalidate();
+        }
+
+
+      //  private void button1_Click(object sender, EventArgs e)
+      //  {  timer1.Start();  }
+
+      //  private void button2_Click(object sender, EventArgs e)
+      //  { timer1.Stop();  }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int k=4;
+            if (e.KeyCode == Keys.Left) {  k = 0; }
+            if (e.KeyCode == Keys.Right) { k = 1; }
+            if (e.KeyCode == Keys.Down) { time++; }
+            if (e.KeyCode == Keys.Up) { k = 3; }
+            block.move(k);
+           
+            e.Handled = true;
+            Invalidate();
+        }
+ 
+        
+      
+    }
+
+}
---
+++ board.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication7
+{
+    class board
+    {
+        public int[,] b1 = new int[,]
+        {   {0,0,0,0},
+            {0,0,0,0},
+            {1,1,1,1},
+            {0,0,0,0}   };
+        public int[,] b2 = new int[,]
+        {
+            {0,0,0,0},
+            {0,2,2,0},
+            {0,2,2,0},
+            {0,0,0,0}    };
+        public int[,] b3 = new int[,]
+        {
+            {0,3,0,0},
+            {0,3,3,0},
+            {0,0,3,0},
+            {0,0,0,0}   };
+        public int[,] b4=new int[,]
+        {
+            {0,0,4,0},
+            {0,4,4,0},
+            {0,4,0,0},
+            {0,0,0,0}   };
+        public int[,] b5= new int[,]
+        {
+            {0,5,5,0},
+            {0,0,5,0},
+            {0,0,5,0},
+            {0,0,0,0}   };
+        public int[,] b6 = new int[,]
+        {
+            {0,6,6,0},
+            {0,6,0,0},
+            {0,6,0,0},
+            {0,0,0,0}   };
+    }
+    class Mainfunction
+    {
+        public int[,] mainB = new int[4, 4];
+        public int i, j;
+        board temp = new board();
+
+        public Mainfunction()
+        {
+            i = 3; j = 0;
+            Random random = new Random();
+            int k = random.Next(1, 7);
+            k = 1;
+            switch (k)
+            {
+                case 1:
+                    mainB = temp.b1;
+                    break;
+                case 2:
+                    mainB = temp.b2;
+                    break;
+                case 3:
+                    mainB = temp.b3;
+                    break;
+                case 4:
+                    mainB = temp.b4;
+                    break;
+                case 5:
+                    mainB = temp.b5;
+                    break;
+                case 6:
+                    mainB = temp.b6;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void put(ref int[,] map, int time, int move)
+        {
+            for (int a = 0; a < 4; a++)
+            {
+                for (int b = 0; b < 4; b++)
+                {
+                    if (map[time - 4 + a, move + b] == 0)
+                    {
+                        map[time - 4 + a, move + b] = map[time - 4 + a, move + b] + mainB[a, b];
+                    }
+                }
+            }
+
+        }
+        public void move(int k)
+        {
+            if (k == 3)
+            {
+                int[,] temp= new int[4,4];
+                for (int a = 0; a < 4; a++)
+                {
+                    for (int b = 0; b < 4; b++)
+                    {
+                        temp[a, b] = mainB[3-b,a];
+                    }
+                }
+                mainB = temp;
+            }
+
+            switch(k)
+            {
+                case 0:
+                    i--;
+                    break;
+                case 1:
+                    i++;
+                    break;
+                case 2:
+                    j++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public int check(int[,] map, int time, int move, int bottom)
+        {
+            int check = 0;
+
+            for (int a = 0; a < 10; a++)
+            {
+                if (map[time - bottom, a] != 0)
+                {
+                    check = 1;
+                }
+            }
+
+            if (bottom > 3)
+            {
+                check = 2;
+                return check;
+            }
+
+            if (check == 1)
+            {
+
+                for (int a = 0; a < 4; a++)
+                {
+
+
+                    //Console.WriteLine(mainB[3 - bottom,i]);
+                    for (int b = 0; b <= bottom; b++)
+                    {
+
+                        if (time > 16)
+                        {
+                            if (map[time - bottom,move +a ] != 0 && mainB[3 - bottom,a ] != 0)
+                            {
+                                check = 2;
+                                return check;
+                            }
+                        }
+                        else if (map[time - b,move +a ] != 0 && mainB[3 - b, a] != 0)
+                        {
+                            check = 2;
+                            return check;
+                        }
+                    }
+
+
+                }
+
+
+            }
+
+            return check;
+        }
+        
+    }
+}
